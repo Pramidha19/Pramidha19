c681ff1983b09313fb824b0b0923e85643c81ffe
Pramidha19 <142799365+Pramidha19@users.noreply.github.com>
2023-09-01 13:54:32 +0530

    Add files via upload

---
+++ bankl.java
@@ -0,0 +1,21 @@
+package xyz;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		int x,y,z,b,d,t,r,ti,tb;
+		b=25000;
+		d=15000;
+		t=11000;
+		r=(b+d)-t;
+		b=t*2/100;
+		ti=12*b;
+		tb=b+ti;
+		{
+			System.out.println(" total balance"+tb);
+		
+		}
+		
+	}
+
+}
