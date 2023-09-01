03fb9777d331ba9edd814424ecb4338597defdcb
Pramidha19 <142799365+Pramidha19@users.noreply.github.com>
2023-09-01 13:53:54 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,18 @@
+package xyz;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		for(int j=0;j<=4; j++)
+		{
+			for(int i=0;i<=j;i++)
+			{
+				System.out.println("*");
+			}
+			
+			System.out.println();
+			
+		}
+	}
+
+}
