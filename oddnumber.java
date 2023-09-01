e65be1931ffd2e428139012b296e2b74972c9d5b
Pramidha19 <142799365+Pramidha19@users.noreply.github.com>
2023-09-01 13:54:12 +0530

    Add files via upload

---
+++ oddnumber.java
@@ -0,0 +1,13 @@
+package xyz;
+
+public class oddnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=1; i<=19; i+=2)
+{
+	System.out.println(i);
+}
+	}
+
+}
