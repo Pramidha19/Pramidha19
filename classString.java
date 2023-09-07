c81998110cfe5f28e3ff3050a59d54d44b2f1b8d
Pramidha19 <142799365+Pramidha19@users.noreply.github.com>
2023-09-07 21:26:05 +0530

    Add files via upload

---
+++ classString.java
@@ -0,0 +1,26 @@
+package homelabs;
+
+public class classstring {
+
+	public static void main(String[] args) {
+		
+		String s1= "pramidha";
+		String s2="satisha";
+		String s3="pramidha";
+		System.out.println(s1.concat(s3));
+		System.out.println(s1.charAt(3));
+		System.out.println(s1.substring(3));
+		System.out.println(s2.replace('s', 'a'));
+		System.out.println(s2.indexOf("s"));
+		System.out.println(s1.startsWith(s3));
+		System.out.println(s1.endsWith(s2));
+		System.out.println(s1.equals(s3));
+		
+		
+		
+		
+// TODO Auto-generated method stub
+
+	}
+
+}
