ffe1015cc66c936eed4a35ecce6772724f66d7f3
Pramidha19 <142799365+Pramidha19@users.noreply.github.com>
2023-09-07 21:25:31 +0530

    Add files via upload

---
+++ stringbuilderstringbuffer.java
@@ -0,0 +1,19 @@
+package homelabs;
+
+public class stringbuilderstringbuffer {
+
+	public static void main(String[] args) {
+		StringBuffer s1=new StringBuffer("pramidha");
+		 StringBuffer s2=new StringBuffer("potti");
+		 StringBuilder s3= new StringBuilder("pramidha");
+		 StringBuilder s4=new StringBuilder("reddy");
+		 System.out.println(s1.reverse());
+		 System.out.println(s1.delete(1,2));
+		 System.out.println(s2.append("potti"));
+		 System.out.println(s2.insert (1,'p'));
+		 System.out.println (s4.length());
+		 // TODO Auto-generated method stub
+
+	}
+
+}
